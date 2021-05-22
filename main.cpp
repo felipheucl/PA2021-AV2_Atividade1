8a4955ba79237b800a6fd0e4444f7eaa3431b524
feliphe <>
2021-05-22 11:51:29 +0000

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,27 @@
+/*
+1) Criar uma função que:
+a. Retorne um valor do tipo inteiro que corresponda ao número de trocas efetuadas pela
+função.
+b. Receba como parâmetros de entrada.
+i. Um ponteiro para um vetor de caracteres que contenha a mensagem.
+ii. Um inteiro que indique o número de elementos do vetor (este parâmetro é
+opcional, você poderá colocá-lo ou não).
+iii. Um caractere a ser substituído.
+iv. Um caractere a ser colocado no lugar do anterior.
+v. Um ponteiro para um vetor de caracteres que deverá conter a nova mensagem.
+
+2) A função main do seu programa, deverá ser capaz de pedir ao usuário que digite uma
+mensagem, um caractere a ser substituído e o caractere pelo qual deve ser substituído. Ao
+final do processamento, as duas mensagens devem ser impressas no console e o número de
+trocas efetuadas.
+*/
+
+#include <iostream>
+#include <fstream>
+#include <string.h>
+using namespace std;
+
+
+int main (){
+
+}
